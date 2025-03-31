48585ecc9429304fce1d12571f19d822557950a8
Fer Arce <farcego@gmail.com>
2025-03-31 18:06:08 -0500

    my package installing code

---
+++ myFiles/current.Rpackages.sh
@@ -0,0 +1,8 @@
+R_LIB=~/HOME-INST/R/$(Rscript -e 'cat(R.version$major,
+R.version$minor, sep=".")')  Rscript -e
+'install.packages(c("Rcpp","TMB","nimble","terra", "sf", "spatstat",
+"sp", "suncalc", "raster", "momentuHMM", "aniMotum", "maps", "magic",
+"gridExtra", "geosphere", "geodist","CircStats", "data.table", "curl",
+"adehabitatHR", "adehabitatLT", "adehabitatMA", "coda", "roxygen2"), type =
+"source", lib = Sys.getenv("R_LIB"), repos =
+"https://cloud.r-project.org")'
