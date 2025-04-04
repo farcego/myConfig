dc62b82088cb56646d817183adeafe72acdceca7
Fer Arce <farcego@gmail.com>
2025-04-04 01:56:39 -0500

    return saved files to their system location

---
+++ returnFiles.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# This file will return the updated programs to its original destination
+
+# 
+cp ~/HOME-INST/myConfig/myFiles/current.emacs.el  ~/emacs.el
+cp ~/HOME-INST/myConfig/myFiles/current.Renviron ~/.Renviron 
+
