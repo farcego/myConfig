9eb286574a670b7d6dcd70c8ee408b27781ef488
Fer Arce <farcego@gmail.com>
2025-04-11 08:50:52 -0500

    circular package added

--- myFiles/current.Rpackages.sh
+++ myFiles/current.Rpackages.sh
@@ -3,5 +3,5 @@ Rscript -e 'install.packages(c("Rcpp","TMB","nimble","terra", "sf", "spatstat",
 "sp", "suncalc", "raster", "momentuHMM", "aniMotum", "maps", "magic",
 "gridExtra", "geosphere", "geodist","CircStats", "data.table", "curl",
 "adehabitatHR", "adehabitatLT", "adehabitatMA", "coda", "roxygen2", "circular",
-"coda", "rjags"), type = "source", lib = Sys.getenv("R_LIB"),
+"coda", "rjags", "plotrix", "circular"), type = "source", lib = Sys.getenv("R_LIB"),
 repos = "https://cloud.r-project.org")'
